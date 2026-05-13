cc814a1ccfa0abef15551eed1c9672a57db52517
VPhuong2K6 <nvphuong2k6@gmail.com>
2026-05-13 21:44:25 +0700

    Lab 6 Vu Phuong & Cong Son

--- tests/test_wrong_key_negative.py
+++ tests/test_wrong_key_negative.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import pytest
 from aes_socket_utils import encrypt_aes_cbc, decrypt_aes_cbc
 
